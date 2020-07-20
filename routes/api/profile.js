3e5fa32947c30a58d234c981237ed8da49c5bb8e
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-07-20 19:24:30 +0430

    Added delete user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -150,4 +150,22 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route       DELETE api/profile/
+// @desc        DELETE profile user and posts
+// @access      Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // @todo remove users posts
+    //Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove USer
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User profile and posts removed" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
