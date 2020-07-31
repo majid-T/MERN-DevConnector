dd96bdc6bcc2c811ec328b89c775375ca40afbfe
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-07-31 21:55:22 +0430

    Added addExperience componnet and redux action

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -45,6 +46,11 @@ const App = () => {
                 exact
                 path="/edit-profile"
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
               />
             </Switch>
           </section>
