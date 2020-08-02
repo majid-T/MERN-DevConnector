11ea7a6bb6cefb04b4e98a96a8aedac763f775ab
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-08-03 03:27:53 +0430

    Added single Post page

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,19 +2,37 @@ import axios from "axios";
 import { setAlert } from "./alert";
 import {
   GET_POSTS,
+  GET_POST,
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
 } from "./types";
 
-//Get Post
+//Get all Posts
 export const getPosts = () => async (dispatch) => {
   try {
     const res = await axios.get("/api/posts");
 
     dispatch({
       type: GET_POSTS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//Get Post
+export const getPost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+
+    dispatch({
+      type: GET_POST,
       payload: res.data,
     });
   } catch (err) {
