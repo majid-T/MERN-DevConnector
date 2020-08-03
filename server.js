af3ecf067d610271530582f49c6dc0ec10cb03ec
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-08-03 05:05:03 +0430

    Made changes for hreoku deploymeny

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 // Requiring routers
 const usersRoute = require("./routes/api/users");
@@ -15,17 +16,23 @@ connectDB();
 // Below is new form of bodyparser
 app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("Express is running");
-});
-
 //Defining routes
 app.use("/api/users", usersRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/profile", profileRoute);
 app.use("/api/posts", postsRoute);
+
+//Serve Static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   `Serveris running on port ${PORT}`;
